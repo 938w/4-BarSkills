1c69de154334dac239017df2ab9515b92b675f31
Sidhu938W <94768561+Sidhu938W@users.noreply.github.com>
2022-03-02 18:28:02 -0800

    new update

---
+++ 4-BarSkills.v5code/src/main.cpp
@@ -0,0 +1,91 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\sidhu                                            */
+/*    Created:      Mon Feb 28 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    1, 3, 15, 4, 20 
+// FourBar              motor         10              
+// ForkLift             motor         5               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+/*class pid {
+  private:
+    void linedrive (double distance, double dir, float velocity, double porportion, double slowdown) {
+      //higher porportion causes the pid "reacting" turning amount to be higher
+      //lower porportion causes the pid "reacting" turning amount to be lower
+      //velocity changes base speed
+      //just set dir to 0
+      //slowdown is the decellaration multiplier, higher it is the more it slows down.
+      Inertial.resetHeading();
+      Inertial.resetRotation();
+      if (velocity > 0) {
+        //driving forward
+        while (((LeftDriveSmart.rotation(rotationUnits::deg)+RightDriveSmart.rotation(rotationUnits::deg))/2)*12.57/360 < distance) {
+          //getting closer
+          if(((LeftDriveSmart.rotation(rotationUnits::deg)+RightDriveSmart.rotation(rotationUnits::deg))/2)*12.57/360 > (distance-6)) {
+
+            //actual pid
+            double out = distance-(((LeftDriveSmart.rotation(rotationUnits::deg)+RightDriveSmart.rotation(rotationUnits::deg))/2)*12.57/360);
+            LeftDriveSmart.spin(vex::directionType::fwd, (velocity-out*slowdown)-(dir-(Inertial.yaw()*porportion)), vex::velocityUnits::pct);
+            RightDriveSmart.spin(vex::directionType::fwd, (velocity-out*slowdown)+(dir-(Inertial.yaw()*porportion)), vex::velocityUnits::pct);
+          } else {
+            //normal
+            //actual pid
+            LeftDriveSmart.spin(vex::directionType::fwd, (velocity-(dir-(Inertial.yaw())*porportion)), vex::velocityUnits::pct);
+            RightDriveSmart.spin(vex::directionType::fwd, (velocity+(dir-(Inertial.yaw())*porportion)), vex::velocityUnits::pct);
+          }
+        }
+      } else {
+        //driving backwards
+        while (((LeftDriveSmart.rotation(rotationUnits::deg)+RightDriveSmart.rotation(rotationUnits::deg))/2)*12.57/360 > distance) {
+          //getting closer 
+          if(((LeftDriveSmart.rotation(rotationUnits::deg)+RightDriveSmart.rotation(rotationUnits::deg))/2)*12.57/360 < (distance+6)) {
+            //actual pid
+            double out = distance-(((LeftDriveSmart.rotation(rotationUnits::deg)+RightDriveSmart.rotation(rotationUnits::deg))/2)*12.57/360);
+            LeftDriveSmart.spin(vex::directionType::fwd, (velocity-out*slowdown)+(dir-(Inertial.yaw()*porportion)), vex::velocityUnits::pct);
+            RightDriveSmart.spin(vex::directionType::fwd, (velocity-out*slowdown)-(dir-(Inertial.yaw()*porportion)), vex::velocityUnits::pct);
+          } else{
+            //normal
+            //actual pid
+            LeftDriveSmart.spin(vex::directionType::fwd, (velocity+(dir-(Inertial.yaw())*porportion)), vex::velocityUnits::pct);
+            RightDriveSmart.spin(vex::directionType::fwd, (velocity-(dir-(Inertial.yaw())*porportion)), vex::velocityUnits::pct);
+          }
+        }
+      }
+      LeftDriveSmart.stop(brakeType::brake);
+      RightDriveSmart.stop(brakeType::brake);
+    }
+  public:
+    void drive (double distance, double dir, float velocity, double porportion, double slowdown) {
+      linedrive(distance,dir,velocity,porportion,slowdown);
+    }
+};
+pid PID;
+*/
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  //Drive to neutral goal ahead
+  Drivetrain.driveFor(forward, 55, inches); 
+  //Clamp goal
+  Clamp.set(false);
+  wait (100, msec); 
+  //Lift the goal slightly (only slightly to prevent excessive tipping)
+  FourBar.spinFor(120, degrees, 100, rpm);
+  //Turn to platform  
+  Drivetrain.turnFor(30, degrees, 200, rpm);
+  Drivetrain.stop();
+  
+
+
+}
