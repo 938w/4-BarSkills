7e80e489ab63102e229312e2156ee4670bbfe220
Sidhu938W <94768561+Sidhu938W@users.noreply.github.com>
2022-03-05 17:31:10 -0800

    yo my bad

--- 4-BarSkills.v5code/src/main.cpp
+++ 4-BarSkills.v5code/src/main.cpp
@@ -10,111 +10,20 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// Drivetrain           drivetrain    1, 3, 15, 4, 20 
-// FourBar              motor         10              
-// ForkLift             motor         5               
+// Drivetrain           drivetrain    1, 3, 15, 4, 20
+// FourBar              motor         10
+// ForkLift             motor         5
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
+#include "pid.h"
+#include "route1.h"
 #include "vex.h"
-
 using namespace vex;
-/*
-class pid {
-  private:
-    void linedrive (double distance, double dir, float velocity, double porportion, double slowdown) {
-      //higher porportion causes the pid "reacting" turning amount to be higher
-      //lower porportion causes the pid "reacting" turning amount to be lower
-      //velocity changes base speed
-      //just set dir to 0
-      //slowdown is the decellaration multiplier, higher it is the more it slows down.
-      Inertial.resetHeading();
-      Inertial.resetRotation();
-      if (velocity > 0) {
-        //driving forward
-        while (((LeftDriveSmart.rotation(rotationUnits::deg)+RightDriveSmart.rotation(rotationUnits::deg))/2)*12.57/3100 < distance) {
-          //getting closer
-          if(((LeftDriveSmart.rotation(rotationUnits::deg)+RightDriveSmart.rotation(rotationUnits::deg))/2)*12.57/3100 > (distance-6)) {
 
-            //actual pid
-            double out = distance-(((LeftDriveSmart.rotation(rotationUnits::deg)+RightDriveSmart.rotation(rotationUnits::deg))/2)*12.57/3100);
-            LeftDriveSmart.spin(reverse, (velocity-out*slowdown)-(dir-(Inertial.yaw()*porportion)), vex::velocityUnits::pct);
-            RightDriveSmart.spin(reverse, (velocity-out*slowdown)+(dir-(Inertial.yaw()*porportion)), vex::velocityUnits::pct);
-          } else {
-            //normal
-            //actual pid
-            LeftDriveSmart.spin(reverse, (velocity-(dir-(Inertial.yaw())*porportion)), vex::velocityUnits::pct);
-            RightDriveSmart.spin(reverse, (velocity+(dir-(Inertial.yaw())*porportion)), vex::velocityUnits::pct);
-          }
-        }
-      } else {
-        //driving backwards
-        while (((LeftDriveSmart.rotation(rotationUnits::deg)+RightDriveSmart.rotation(rotationUnits::deg))/2)*12.57/3100 > distance) {
-          //getting closer 
-          if(((LeftDriveSmart.rotation(rotationUnits::deg)+RightDriveSmart.rotation(rotationUnits::deg))/2)*12.57/3100 < (distance+6)) {
-            //actual pid
-            double out = distance-(((LeftDriveSmart.rotation(rotationUnits::deg)+RightDriveSmart.rotation(rotationUnits::deg))/2)*12.57/3100);
-            LeftDriveSmart.spin(reverse, (velocity-out*slowdown)+(dir-(Inertial.yaw()*porportion)), vex::velocityUnits::pct);
-            RightDriveSmart.spin(reverse, (velocity-out*slowdown)-(dir-(Inertial.yaw()*porportion)), vex::velocityUnits::pct);
-          } else{
-            //normal
-            //actual pid
-            LeftDriveSmart.spin(reverse, (velocity+(dir-(Inertial.yaw())*porportion)), vex::velocityUnits::pct);
-            RightDriveSmart.spin(reverse, (velocity-(dir-(Inertial.yaw())*porportion)), vex::velocityUnits::pct);
-          }
-        }
-      }
-      LeftDriveSmart.stop(brakeType::brake);
-      RightDriveSmart.stop(brakeType::brake);
-    }
-  public:
-    void drive (double distance, double dir, float velocity, double porportion, double slowdown) {
-      linedrive(distance,dir,velocity,porportion,slowdown);
-    }
-};
-pid PID;
-*/
 int main() {
   // // Initializing Robot Configuration. DO NOT REMOVE!
- vexcodeInit();
-  Brain.Screen.print("att2");
-  Mobile.Screen.print(Inertial.heading());
-  //Set Velocity
-  leftMotorA.setVelocity(100, percent);
-  leftMotorB.setVelocity(100, percent);
-  rightMotorA.setVelocity(100, percent);
-  rightMotorB.setVelocity(100, percent);
-  //Lower fork lift
-  ForkLift.spinFor(reverse, 550, degrees, 100, rpm); 
-  // Drive to blue goal
-  Drivetrain.driveFor(forward, 15.5, inches, 140, rpm);
-  Drivetrain.stop();
-  //Pick up
-  ForkLift.spinFor(forward, 350, degrees, 100, rpm); 
-  //drive backwards a little bit
-  Drivetrain.driveFor(reverse, 16, inches, 140, rpm);
-  //Turn 90 degrees to face neutral goal
-  Drivetrain.turnToHeading(110, degrees, 100, rpm);
-  //Activate clamp
-  Clamp.set(true); 
-  //Drive to neutral goal
-  Drivetrain.driveFor(reverse, 30, inches, 175, rpm);
-  Drivetrain.driveFor(reverse, 10, inches, 90, rpm);
-  Drivetrain.driveFor(reverse, 10, inches, 50, rpm);
-  Drivetrain.stop(brake);
-  wait(0.2, sec);
-  //Clamp neutral
-  Clamp.set(false);
-  //go to platform
-  FourBar.spinFor(forward, 700, degrees, 100, rpm, false);
-  Drivetrain.driveFor(reverse, 35, inches, 170 ,rpm);
-  Drivetrain.driveFor(reverse, 10, inches, 70, rpm);
-  Drivetrain.driveFor(reverse, 10, inches, 50, rpm);
-  FourBar.spinFor(reverse, 50, degrees, false);
+  vexcodeInit();
 
-
-  
-
-  
-
-
+  Route1 route1;
+  route1.run();
 }
--- 4-BarSkills.v5code/src/route1.cpp
+++ 4-BarSkills.v5code/src/route1.cpp
@@ -2,30 +2,78 @@
 #include "vex.h"
 
 using namespace vex;
-
+timer t1;
 void Route1::run() {
 
   Brain.Screen.print("route 1");
-
+  Brain.Screen.print("att2");
   Mobile.Screen.print(Inertial.heading());
-  // Set Velocity
+  //Set Velocity
   leftMotorA.setVelocity(100, percent);
   leftMotorB.setVelocity(100, percent);
   rightMotorA.setVelocity(100, percent);
   rightMotorB.setVelocity(100, percent);
-  // Lower fork lift
-  ForkLift.spinFor(reverse, 1100, degrees, 100, rpm);
+  //Lower fork lift
+  ForkLift.spinFor(reverse, 550, degrees, 100, rpm); 
   // Drive to blue goal
-  Drivetrain.driveFor(forward, 16, inches, 140, rpm);
+  Drivetrain.driveFor(forward, 15.5, inches, 140, rpm);
   Drivetrain.stop();
-  // Pick up
-  ForkLift.spinFor(forward, 500, degrees, 100, rpm);
-  // Turn 90 degrees to face neutral goal
-  Drivetrain.turnFor(left, 270, degrees, 100, rpm);
-  // Activate clamp
-  Clamp.set(true);
-  // Drive to neutral goal
-  Drivetrain.driveFor(reverse, 20, inches, 175, rpm);
-  // Clamp neutral
+  //Pick up
+  ForkLift.spinFor(forward, 350, degrees, 100, rpm); 
+  //drive backwards a little bit
+  Drivetrain.driveFor(reverse, 16, inches, 140, rpm);
+  //Turn 90 degrees to face neutral goal
+  Drivetrain.turnToHeading(108, degrees, 100, rpm);
+  //Activate clamp
+  Clamp.set(true); 
+  
+  //Drive to neutral goal
+  Drivetrain.driveFor(reverse, 50, inches, 100, rpm);
+  
+  Drivetrain.stop(brake);
+  wait(0.2, sec);
+  //Clamp neutral
   Clamp.set(false);
+  //go to platform
+  FourBar.spinFor(forward, 770, degrees, 100, rpm, false);
+  Drivetrain.turnFor(right, 10, degrees);
+  wait(0.2, sec);
+  Drivetrain.driveFor(reverse, 54, inches, 100,rpm);
+  FourBar.spinFor(reverse, 75, degrees, false);
+  //Turn so goal is closer to the platforms center of gravity
+  //Drivetrain.turnFor(right, 25, degrees); 
+  //Lower fourbar so goal doesn't tip over while dropping
+  FourBar.spinFor(reverse, 240, degrees); 
+  //Release
+  Clamp.set(true); 
+  //getbluegoal
+  t1.reset();
+    ForkLift.spinFor(reverse, 340, degrees, 100 ,rpm, false);
+    Drivetrain.driveFor(forward, 10, inches, 200, rpm);
+    FourBar.spinFor(reverse, 500, degrees, false);
+    Drivetrain.driveFor(forward, 10, inches, 175, rpm);
+    Drivetrain.stop(brake);
+    wait(0.2, sec);
+    Drivetrain.driveFor(reverse, 10, inches, 175, rpm);
+    Drivetrain.turnFor(180, degrees, 170, rpm);
+    Drivetrain.driveFor(reverse, 12, inches, 175, rpm); 
+    Mobile.Screen.newLine();
+    Mobile.Screen.print(t1.time(sec));
+
+  //clamp
+  Clamp.set(false);
+  FourBar.spinFor(forward, 200, degrees);
+  //turn to red
+  Drivetrain.turnToHeading(-160, degrees, 150, rpm);
+  //drive to red
+  Drivetrain.driveFor(forward, 20, inches, 175, rpm);
+  //pic up red
+  ForkLift.spinFor(forward, 350, degrees, 100, rpm, false);
+  wait(0.5, sec);
+  Drivetrain.driveFor(reverse, 20, inches, 100, rpm);
+
+
+
+
+  
 }
